dd778450585222124cf30967bdae0b38be321be7
Helena Sánchez <helena.sangar@gmail.com>
2023-06-08 22:22:38 +0200

    jueves noche

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -36,15 +36,6 @@ const getOneUser = (req, res, next) => {
 }
 
 
-/* const getOwnedJobs = (req, res, next) => {
-
-    User
-        .find( )
-        .select({ username: 1, avatar: 1, role: 1, location: 1, availability: 1, jobCategory: 1 })
-        .sort({ username: 1 })
-        .then(users => res.json(users))
-        .catch(err => next(err));
-}; */
 
 const getCandidateUsers = (req, res, next) => {
 
@@ -58,15 +49,34 @@ const getCandidateUsers = (req, res, next) => {
 
 
 const editUser = (req, res, next) => {
+    const { email, username, role, avatar, description, location, jobCategory, yearsOfExperience, availability, travelAvailability, languages, skills, dailyRate, grossSalary, experience, savedJob, favoriteProfessionals } = req.body;
+    const { id } = req.params;
 
-    const { email, username, role, avatar, description, location, jobCategory, yearsOfExperience, availability, travelAvailability, languages, skills, dailyRate, grossSalary, experience, savedJob, favoriteProfessionals } = req.body
-    const { id } = req.params
+    const updatedUser = {
+        email,
+        username,
+        role,
+        avatar,
+        description,
+        location,
+        jobCategory,
+        yearsOfExperience,
+        availability,
+        travelAvailability,
+        languages,
+        skills,
+        dailyRate,
+        grossSalary,
+        experience,
+        savedJob,
+        favoriteProfessionals
+    };
 
-    User
-        .findByIdAndUpdate(id, { email, username, role, avatar, description, location, jobCategory, yearsOfExperience, availability, travelAvailability, languages, skills, dailyRate, grossSalary, experience, savedJob, favoriteProfessionals }, { new: true })
+    User.findByIdAndUpdate(id, updatedUser, { new: true })
         .then(response => res.json(response))
-        .catch(err => next(err))
-}
+        .catch(err => next(err));
+};
+
 
 
 const addSavedJob = (req, res, next) => {
